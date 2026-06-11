753b5591f8b59a33af8410dec30978cd6d5fba88
Abdalrhman Almustafa <161914020+DarkZero81@users.noreply.github.com>
2026-06-11 15:36:16 +0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,13 +23,16 @@ class UserFactory extends Factory
      */
 public function definition()
 {
+    // استدعاء الفيكر عبر الـ Container الخاص بلارافيل لضمان عمله 100%
+    $faker = \Faker\Factory::create();
+
     return [
-        'name' => $this->faker->name(),
-        'email' => $this->faker->unique()->safeEmail(),
+        'name'              => $faker->name(),
+        'email'             => $faker->unique()->safeEmail(),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ]; // تأكد أن الإغلاق سليم ولا توجد فاصلة غريبة هنا بالأسفل
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token'    => \Illuminate\Support\Str::random(10),
+    ];
 }
 
     /**
